70c266a9e73c561eaef2f1322955b1b5e1fa26e9
Kevin Sun <kevin.sun@student.allenisd.org>
2022-11-17 12:08:24 -0600

    Code for C101 and C102

---
+++ C101 Factorial [Swift]/main.swift
@@ -0,0 +1,27 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+func factorial(n: Int) -> Int {
+    print("Calculating factorial(\(n))...")
+    if n <= 1 {
+        let answer = 1
+        print("Terminal case, returning \(answer).")
+        return 1
+    }
+    let answer = n * factorial(n: n - 1)
+    
+        print("Non-terminal case, returning \(answer).")
+        return answer
+}
+
+print("The final result is: \(factorial(n:n)).")
+
+
